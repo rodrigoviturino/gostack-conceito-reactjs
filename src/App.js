2c2460408d35aa78b2339692472c92ff6ac426bf
Rodrigo Viturino <rodrigo.souza@spread.com.br>
2021-05-22 15:00:35 -0300

    Criando métodos GET, POST E DELETE e aplicando teste unitario

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import React from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = React.useState([]);
+
+  React.useEffect(() => {
+    api.get('repositories').then( (response) => { 
+      setRepositories(response.data) 
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const addNew = await api.post('repositories', {
+      title: 'FullStack',
+      url: 'https://github.com/rodrigoviturino',
+      techs: ['JS', 'HTML', 'CSS']
+    })
+
+    setRepositories([...repositories, addNew.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const removeItem= repositories.filter(repository => repository.id !== id)
+    setRepositories(removeItem);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => {
+          return(
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+        )})}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
+
+
 
 export default App;
